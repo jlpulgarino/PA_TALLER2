5347ceb4d0e3ef7b7e6da46bc777af7eb5ec39b7
Jose Luis Pulgarin <jl.pulgarin@uniandes.edu.co>
2019-01-31 23:52:25 -0500

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -129,6 +129,26 @@
     app.getSchedule = function (key, label) {
         var url = 'https://api-ratp.pierre-grimaud.fr/v3/schedules/' + key;
 
+		if ('caches' in window) {
+		  /*
+		   * Check if the service worker has already cached this city's weather
+		   * data. If the service worker has the data, then display the cached
+		   * data while the app fetches the latest data.
+		   */
+		  caches.match(url).then(function(response) {
+			if (response) {
+			  response.json().then(function updateFromCache(json) {
+				var results = json.query.results;
+				results.key = key;
+				results.label = label;
+				results.created = json.query.created;
+                app.updateTimetableCard(results);
+			  });
+			}
+		  });
+		}
+		
+		
         var request = new XMLHttpRequest();
         request.onreadystatechange = function () {
             if (request.readyState === XMLHttpRequest.DONE) {
